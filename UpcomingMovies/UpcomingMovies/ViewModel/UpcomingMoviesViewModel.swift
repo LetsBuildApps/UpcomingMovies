87e331386c410037b2101e59147354041422c4dd
Charnpreet <sony_baf@yahoo.co.in>
2021-08-04 16:55:07 +1000

    added view model for upcoming movies

---
+++ UpcomingMovies/UpcomingMovies/ViewModel/UpcomingMoviesViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingMoviesViewModel.swift
+//  UpcomingMovies
+//
+//  Created by CHARNPREET SINGH on 4/8/21.
+//
+
+import Foundation
+
+
+/// will be used by UpcomingMovies ViewHandler
+struct UpcomingMoviesViewModel {
+    /*
+     * should have property for image
+     * should have property for  ratings
+     * should have property for title
+     * should have property for relase date
+     */
+    var posterPath: String
+    var title: String
+    var release_date: String
+    var rating: Float
+}
