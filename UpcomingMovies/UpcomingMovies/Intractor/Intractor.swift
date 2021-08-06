91afdada8c69d2254bd21a94450f5fd55c8aff79
Charnpreet <sony_baf@yahoo.co.in>
2021-08-06 13:37:10 +1000

    added error inside the call back

--- UpcomingMovies/UpcomingMovies/Intractor/Intractor.swift
+++ UpcomingMovies/UpcomingMovies/Intractor/Intractor.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Intractor: Intractable{
+class Intractor: Intractable {
     var decoder: DataDecoder
     var fetcher: DataLoader
     init(decoder: DataDecoder, fetcher: DataLoader) {
@@ -15,29 +15,32 @@ class Intractor: Intractable{
         self.fetcher = fetcher
     }
     func loadData(using url: URL, callback: @escaping ([ViewModel]?) -> ()) {
-        loadDataFromAPI(from: url, callback: {viewModel in
+        loadDataFromAPI(from: url, callback: { viewModel, error in
             callback(viewModel)
         })
     }
     
-  private func loadDataFromAPI(from url: URL,  callback: @escaping ([ViewModel]?) -> ()) {
+  private func loadDataFromAPI(from url: URL,  callback: @escaping ([ViewModel]?, Error?) -> ()) {
         fetcher.load(from: url, resultHandler: { data, error in
-            guard let data = data else {return}
+            guard let data = data else {
+                callback(nil,error)
+                return
+            }
             let result =  self.loadDataFromDecoder(dataToBeDecoder: data)
-            callback(result)
+             callback(result, nil)
         })
     }
   private func loadDataFromDecoder(dataToBeDecoder data: Data) -> [ViewModel]? {
         do {
-            let v: UpcomingMovies = try decoder.decode(dataToBeDecoed: data)
+            let v: Movies = try decoder.decode(dataToBeDecoed: data)
             return buildViewModels(from: v)
         } catch let error {
-            print(error)
+            print("failed here \(self) with error: \(error)")
         }
         return nil
     }
     
-   private func buildViewModels(from model: UpcomingMovies) -> [ViewModel] {
-      return  model.results.map({UpcomingMoviesViewModel(posterPath: $0.posterPath, title: $0.title, release_date: $0.releaseDate, rating: $0.voteAverage)})
+   private func buildViewModels(from model: Movies) -> [ViewModel] {
+      return  model.results.map({UpcomingMovies(posterPath: $0.posterPath, title: $0.title, release_date: $0.releaseDate, rating: $0.voteAverage)})
     }
 }
