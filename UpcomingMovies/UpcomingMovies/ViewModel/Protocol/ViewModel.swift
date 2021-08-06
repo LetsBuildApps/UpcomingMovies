2d6839a4cfd9c702c84cf5055ae95e16ab18507c
Charnpreet <sony_baf@yahoo.co.in>
2021-08-06 13:40:25 +1000

    added viewModel protocol

---
+++ UpcomingMovies/UpcomingMovies/ViewModel/Protocol/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  UpcomingMovies
+//
+//  Created by CHARNPREET SINGH on 6/8/21.
+//
+
+import Foundation
+/*
+ * view model protcol is what a view is expecting
+ */
+protocol ViewModel {
+    var posterPath: String {get set}
+    var title: String {get set}
+    var release_date: String {get set}
+    var rating: Double {get set}
+}
