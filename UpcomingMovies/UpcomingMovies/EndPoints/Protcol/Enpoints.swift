67c642f2fb699545ebcbbfe11b5c0125842bb433
Charnpreet <sony_baf@yahoo.co.in>
2021-08-09 13:42:55 +1000

    removed un-necessary properties

--- UpcomingMovies/UpcomingMovies/EndPoints/Protcol/Enpoints.swift
+++ UpcomingMovies/UpcomingMovies/EndPoints/Protcol/Enpoints.swift
@@ -8,12 +8,5 @@
 import Foundation
 
 protocol EndPoint {
-    var path: Path {get}
-    var section: Section {get}
-    var apiKey: String {get}
-    var language: String? {get}
-    var page: Int? {get}
-    var region: String? {get}
-    var queryItems: [URLQueryItem] {get}
     var url: URL {get}
 }
