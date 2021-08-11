9ca269016b547b120bb376fd1abad14d2e3ceab1
Charnpreet <sony_baf@yahoo.co.in>
2021-08-11 13:35:29 +1000

    removed unavailable properties

--- UpcomingMovies/UpcomingMovies/CollectionCell/CellConfigurator/Configurator.swift
+++ UpcomingMovies/UpcomingMovies/CollectionCell/CellConfigurator/Configurator.swift
@@ -14,7 +14,5 @@ protocol CellConfigurable {
 struct CellConfigurator: CellConfigurable {
     func configure(CellToBeConfigured cell: CollectionViewCell, with item: ViewModel) {
         cell.posterImageView.sd_setImage(with: item.posterPath, placeholderImage: UIImage(named: "placeholder.png"))
-        cell.titleLabel.text = item.title
-        cell.ratingLabel.text = "\(item.rating)"
     }
 }
