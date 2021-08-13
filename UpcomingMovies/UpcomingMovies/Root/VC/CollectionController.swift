ed3e3db56d876759a477f9fb127f7f2b3574d59e
Charnpreet <sony_baf@yahoo.co.in>
2021-08-13 12:33:39 +1000

    now using diffable data source to update collection view items also made  generic to use it anywhere

--- UpcomingMovies/UpcomingMovies/Root/VC/CollectionController.swift
+++ UpcomingMovies/UpcomingMovies/Root/VC/CollectionController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
-class CollectionController: UICollectionViewController {
+class CollectionController<T: ViewModel>: UICollectionViewController, UICollectionViewDataSourcePrefetching {
     fileprivate var cellConfigurator: CellConfigurable
     fileprivate var intractor: Intractable
-    fileprivate var sections: [Section] = []
+//    fileprivate var sections: [Section] = []
+    fileprivate var sections: [CellSection<T>] = []
     fileprivate var layout: UICollectionViewCompositionalLayout
     //MARK: - initlizer properties
     init(cellConfigurator: CellConfigurable, intractor: Intractable, layout: UICollectionViewCompositionalLayout) {
         self.cellConfigurator = cellConfigurator
         self.intractor = intractor
         self.layout = layout
-        super.init(nibName: nil, bundle: nil)
+        super.init(collectionViewLayout: layout)
+      //  super.init(nibName: nil, bundle: nil)
 
     }
     
@@ -31,55 +33,65 @@ class CollectionController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented. Please use other intilizers")
     }
+    private var dataSource: DataSource!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = makeDataSource()
+        configureCollectionView()
+        getMovies()
+    }
 
-    fileprivate func getMovies() {
-        intractor.getMovies(callback: {result, error in
-            guard let result = result else {
-                print(error as Any)
-                return
-            }
-            self.sections.append(contentsOf: result.map({$0}))
-            self.collectionView.reloadData()
-        })
+    typealias DataSource = UICollectionViewDiffableDataSource<CellSection<T>, T>
+    func makeDataSource() -> DataSource {
+      // 1
+      let dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, movies) -> UICollectionViewCell? in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell else {return UICollectionViewCell()}
+            self.cellConfigurator.configure(CellToBeConfigured: cell, with: movies)
+            return cell
+      })
+      return dataSource
+    }
+    typealias Snapshot = NSDiffableDataSourceSnapshot<CellSection<T>, T>
+    func applySnapshot(animatingDifferences: Bool = true) {
+      var snapshot = Snapshot()
+        snapshot.appendSections(sections)
+        sections.forEach { section in
+            snapshot.appendItems(section.rows, toSection: section)
+        }
+      dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
     }
     
     fileprivate func configureCollectionView() {
         self.collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         self.collectionView.prefetchDataSource = self
         self.collectionView.collectionViewLayout = layout
+        self.collectionView.dataSource = dataSource
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureCollectionView()
-        getMovies()
-    }
-    
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return sections.count
-    }
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sections[section].rows.count
-    }
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell else {return UICollectionViewCell()}
-        
-        let movies = self.sections[indexPath.section].rows[indexPath.row]
-        cellConfigurator.configure(CellToBeConfigured: cell, with: movies)
-        return cell
-    }
-    
-}
 
-extension CollectionController: UICollectionViewDataSourcePrefetching {
+    fileprivate func getMovies() {
+        intractor.getMovies(callback: {(result:[CellSection<T>]?, error) in
+            guard let result = result else {
+                print(error as Any)
+                return
+            }
+            self.sections.append(contentsOf: result.map({$0}))
+            self.applySnapshot()
+        })
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        guard let video = dataSource.itemIdentifier(for: indexPath) else {
+//          return
+//        }
+    }
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths {
-            if indexPath.row > sections[indexPath.section].rows.count - 2 {
+            if indexPath.item == sections[indexPath.section].rows.count - 5 {
                 getMovies()
             }
         }
     }
 }
-
-
 
