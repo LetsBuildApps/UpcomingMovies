24587baa7181c24ecd38f1ddcbb7ad9d9ea2826d
Charnpreet <sony_baf@yahoo.co.in>
2021-08-13 12:38:40 +1000

    now it is a generic function which accepts type of P

--- UpcomingMovies/UpcomingMovies/CollectionCell/CellConfigurator/Configurator.swift
+++ UpcomingMovies/UpcomingMovies/CollectionCell/CellConfigurator/Configurator.swift
@@ -8,11 +8,20 @@
 
 import SDWebImage
 protocol CellConfigurable {
-    func configure(CellToBeConfigured cell: CollectionViewCell, with item: ViewModel)
+//    func configure(CellToBeConfigured cell: CollectionViewCell, with item: ViewModel)
+    func configure<T:ViewModel>(CellToBeConfigured cell: CollectionViewCell, with item: T)
 }
 
 struct CellConfigurator: CellConfigurable {
-    func configure(CellToBeConfigured cell: CollectionViewCell, with item: ViewModel) {
-        cell.posterImageView.sd_setImage(with: item.posterPath, placeholderImage: UIImage(named: "placeholder.png"))
+//    func configure(CellToBeConfigured cell: CollectionViewCell, with item: ViewModel) {
+//        cell.posterImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+//        cell.posterImageView.sd_imageTransition = .fade
+//        cell.posterImageView.sd_setImage(with: item.posterPath, placeholderImage: nil)
+//    }
+    
+    func configure<T:ViewModel>(CellToBeConfigured cell: CollectionViewCell, with item: T) {
+        cell.posterImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        cell.posterImageView.sd_imageTransition = .fade
+        cell.posterImageView.sd_setImage(with: item.posterPath, placeholderImage: nil)
     }
 }
